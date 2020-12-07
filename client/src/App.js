77ee130a42507c1978357a6920130ed1ce3a0857
Osama-Hamed <osamahamedmohammed@gmail.com>
2020-12-07 11:43:09 +0200

    create header and footer components

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,17 @@
-const App = () => {
-  return <div className='App'>ProShop</div>;
-};
+import Header from './components/Header';
+import Footer from './components/Footer';
+import { Container } from 'react-bootstrap';
+
+const App = () => (
+  <>
+    <Header />
+    <main className='py-3'>
+      <Container>
+        <h1>Welcome to ProShop</h1>
+      </Container>
+    </main>
+    <Footer />
+  </>
+);
 
 export default App;
