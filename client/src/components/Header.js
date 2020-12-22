cae2ca6b06146234d616818d143ad174f3ccde82
Osama-Hamed <osamahamedmohammed@gmail.com>
2020-12-22 17:30:05 +0200

    add logout functionality

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,32 +1,49 @@
+import { useDispatch, useSelector } from 'react-redux';
 import { LinkContainer } from 'react-router-bootstrap';
-import { Container, Navbar, Nav } from 'react-bootstrap';
+import { Container, Navbar, Nav, NavDropdown } from 'react-bootstrap';
+import * as userLoginActions from '../state/userLogin/actions';
 import routes from '../shared/routes';
 
-const Header = () => (
-  <header>
-    <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
-      <Container>
-        <LinkContainer to={routes.homeScreen.path()}>
-          <Navbar.Brand>ProShop</Navbar.Brand>
-        </LinkContainer>
-        <Navbar.Toggle aria-controls='basic-navbar-nav' />
-        <Navbar.Collapse id='basic-navbar-nav'>
-          <Nav className='ml-auto'>
-            <LinkContainer to={routes.cartScreen.path('')}>
-              <Nav.Link>
-                <i className='fas fa-shopping-cart'></i> Cart
-              </Nav.Link>
-            </LinkContainer>
-            <LinkContainer to='/login'>
-              <Nav.Link>
-                <i className='fas fa-user'></i> Sign In
-              </Nav.Link>
-            </LinkContainer>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
-  </header>
-);
-
+const Header = () => {
+  const dispatch = useDispatch();
+  const { authUser } = useSelector(state => state.userLogin);
+  const logout = () => {
+    dispatch(userLoginActions.logout());
+  };
+  return (
+    <header>
+      <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
+        <Container>
+          <LinkContainer to={routes.homeScreen.path()}>
+            <Navbar.Brand>ProShop</Navbar.Brand>
+          </LinkContainer>
+          <Navbar.Toggle aria-controls='basic-navbar-nav' />
+          <Navbar.Collapse id='basic-navbar-nav'>
+            <Nav className='ml-auto'>
+              <LinkContainer to={routes.cartScreen.path('')}>
+                <Nav.Link>
+                  <i className='fas fa-shopping-cart'></i> Cart
+                </Nav.Link>
+              </LinkContainer>
+              {authUser ? (
+                <NavDropdown title={authUser.name} id='username'>
+                  <LinkContainer to='/profile'>
+                    <NavDropdown.Item>Profile</NavDropdown.Item>
+                  </LinkContainer>
+                  <NavDropdown.Item onClick={logout}>Logout</NavDropdown.Item>
+                </NavDropdown>
+              ) : (
+                <LinkContainer to='/login'>
+                  <Nav.Link>
+                    <i className='fas fa-user'></i> Sign In
+                  </Nav.Link>
+                </LinkContainer>
+              )}
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
+  );
+};
 export default Header;
--- client/src/state/userLogin/actions.js
+++ client/src/state/userLogin/actions.js
@@ -31,3 +31,8 @@ export const login = (email, password) => async dispatch => {
     );
   }
 };
+
+export const logout = () => dispatch => {
+  localStorage.removeItem('authUser');
+  dispatch({ type: actionTypes.USER_LOGOUT });
+};
